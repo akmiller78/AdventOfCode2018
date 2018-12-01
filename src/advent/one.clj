96e71b84e325121d0c08629088478328a43fbb65
Adam Miller <akmiller@gmail.com>
2018-12-01 15:22:33 -0600

    adding day one solutions

---
+++ src/advent/one.clj
@@ -0,0 +1,66 @@
+(ns advent.one
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+
+(def frequency-input-file "frequency_input.txt")
+
+
+(defn get-frequencies
+  [file-name]
+  (->> file-name
+       io/resource
+       slurp
+       str/split-lines
+       (map read-string)))
+
+
+(defn total-frequencies
+  [frequencies]
+  (reduce + 0 frequencies))
+
+
+(defn first-repeated-frequency
+  [frequencies]
+  (loop [input (cycle frequencies)
+         frequency 0
+         seen #{}]
+    (let [frequency (+ frequency (first input))]
+      (if (contains? seen frequency)
+        frequency
+        (recur (rest input) frequency (conj seen frequency))))))
+
+
+(defn goal-one
+  []
+  (total-frequencies (get-frequencies frequency-input-file)))
+
+
+(defn goal-two
+  []
+  (first-repeated-frequency (get-frequencies frequency-input-file)))
+
+
+
+(comment
+
+  (goal-one)
+
+  (goal-two)
+
+  )
+
+(comment
+
+  (def data (get-frequencies frequency-input-file))
+
+  (loop [input (cycle data)
+         frequency 0
+         seen #{}]
+    (let [frequency (+ frequency (first input))]
+      (if (contains? seen frequency)
+        frequency
+        (recur (rest input) frequency (conj seen frequency)))))
+
+
+  )
