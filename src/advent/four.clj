82091bf9ce4dc336c9542ccf6efc052ebb5404e1
Adam Miller <akmiller@gmail.com>
2018-12-04 15:06:26 -0600

    clean up code a bit

--- src/advent/four.clj
+++ src/advent/four.clj
@@ -4,48 +4,49 @@
 
 (def input (-> "guard_sleep_patterns.txt" io/resource io/reader line-seq))
 
+(defn str->int [s]
+  (Integer/parseInt s))
+
 (defn ->time-log-entry
+  "Returns log entry as vector of 3 items:
+  timestamp (as string), minute, action"
   [input-str]
   (let [[year month day hour minute action]
         (rest (re-find #"(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (.*)" input-str))]
-    [(read-string (str year month day hour minute))
-     (Integer/parseInt hour)
-     (Integer/parseInt minute)
+    [(str year month day hour minute)
+     (str->int minute)
      action]))
 
-;; associate the guard with the log (rather not use state so investigate this later)
-(defn tag-logs
+(defn assoc-guard
+  "Helper function to assign guard to all log entries"
   [log]
-  (let [id (atom nil)]
-    (map #(let [[_ next-id] (re-find #"Guard #(\d+) begins shift" (last %))
-                _ (when next-id (reset! id next-id))]
-            (into [@id] %))
-         log)))
+  (last
+   (reduce (fn [[id guard-log] entry]
+             (let [id (or (last (re-find #"Guard #(\d+) begins shift" (last entry))) id)]
+               [id (conj guard-log (into [id] entry))]))
+           [nil []]
+           log)))
 
-(def log
-  (->> input
-       (map ->time-log-entry)
-       (sort-by first)
-       tag-logs))
-
-(def falls-asleep-or-wakes (filter #(or (= "falls asleep" (nth % 4))
-                                        (= "wakes up" (nth % 4)))))
-
-(def sleep-log (transduce falls-asleep-or-wakes conj log))
+(def sleep-log
+  (memoize
+   #(->> input
+         (map ->time-log-entry)
+         (sort-by first)
+         (assoc-guard)
+         (filter (fn [entry]
+                   (or (= "falls asleep" (nth entry 3))
+                       (= "wakes up" (nth entry 3))))))))
 
 (defn most-minutes-slept
   [sleep-log]
   (->> sleep-log
        (partition 2)
        (map (fn [[sleep wake]]
-              [(first sleep) (- (nth wake 3) (nth sleep 3))]))
+              [(first sleep) (- (nth wake 2) (nth sleep 2))]))
        (group-by first)
        (map (fn [[id entries]]
               {:id id :slept (reduce + (map last entries))}))
-       (sort-by :slept)
-       (reverse)
-       (take 1)
-       first
+       (apply max-key :slept)
        :id))
 
 (defn longest-minute-slept
@@ -54,36 +55,34 @@
        (filter #(= guard (first %)))
        (partition 2)
        (reduce (fn [minutes [sleep wake]]
-                 (let [slept (range (nth sleep 3) (nth wake 3))]
+                 (let [slept (range (nth sleep 2) (nth wake 2))]
                    (conj minutes slept)))
                [])
        (apply concat)
        (frequencies)
-       (sort-by val)
-       (reverse)
-       (take 1)
-       first))
+       (apply max-key val)))
 
 (defn solve-1
   []
-  (let [guard (most-minutes-slept sleep-log)
-        minute (first (longest-minute-slept sleep-log guard))]
-    (* (read-string guard) minute)))
+  (let [guard (most-minutes-slept (sleep-log))
+        minute (first (longest-minute-slept (sleep-log) guard))]
+    (* (str->int guard) minute)))
 
 (defn solve-2
   []
-  (let [ids (distinct (map first sleep-log))
+  (let [ids (distinct (map first (sleep-log)))
         info
         (->> ids
-             (map #(let [minute (longest-minute-slept sleep-log %)]
+             (map #(let [minute (longest-minute-slept (sleep-log) %)]
                      {:guard % :minute (first minute) :time (last minute)}))
              (sort-by :time)
              (reverse)
              (take 1)
              first)]
-    (* (read-string (:guard info)) (:minute info))))
+    (* (str->int (:guard info)) (:minute info))))
 
 (comment
+
   (time (solve-1))
   (time (solve-2))
 
