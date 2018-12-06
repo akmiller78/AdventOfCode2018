bc0ef28c019ea94709e6c556ad3103c2f37d2148
Adam Miller <akmiller@gmail.com>
2018-12-05 21:48:03 -0600

    fix perf. of solution 2

--- src/advent/five.clj
+++ src/advent/five.clj
@@ -9,30 +9,21 @@
   (if (or (nil? b1) (nil? b2))
     false
     (and (not= b1 b2)
-         (= (bit-clear b1 5) (bit-clear b2 5)))))
+         (== (bit-clear b1 5) (bit-clear b2 5)))))
 
 (defn react-polymer
-  ([input] (react-polymer input nil))
-  ([input exclusions]
-   (reduce #(let [last-unit (peek %1)]
-              (if (and (not (empty? exclusions)) (contains? exclusions %2))
-                %1
-                (if (opposite-polarity? last-unit %2)
-                  (pop %1)
-                  (conj %1 %2)))) [] input)))
+  [input]
+  (reduce #(let [last-unit (peek %1)]
+             (if (opposite-polarity? last-unit %2)
+               (pop %1)
+               (conj %1 %2))) [] input))
 
 (defn solve-1 []
   (count (react-polymer data)))
 
 (defn solve-2 []
-  (reduce (fn [cur-min drop-unit]
-            (let [exclusions #{drop-unit (bit-flip drop-unit 5)}
-                  cnt (count (react-polymer data exclusions))]
-              (if (nil? cur-min)
-                cnt
-                (min cur-min cnt))))
-          nil
-          (range 65 91)))
-
-(time (solve-1))
-(time (solve-2))
+  (apply min (pmap (fn [char]
+                    (let [input (remove #(or (= % char)
+                                             (= % (+ char 32))) data)]
+                      (count (react-polymer input))))
+                  (range 65 91))))
